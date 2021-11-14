f8561effa380b332f83a71d47d11cd43e6c50aa4
momenasuallam <59420500+momenasuallam@users.noreply.github.com>
2021-11-14 21:20:27 +0500

    setup

---
+++ js/app.js
@@ -0,0 +1,16 @@
+window.addEventListener("scroll", () => {
+  let header = document.querySelector(".header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+$(document).ready(function () {
+  // Toggle menu on click
+  $("#menu-toggler").click(function () {
+    toggleBodyClass("menu-active");
+  });
+
+  function toggleBodyClass(className) {
+    document.body.classList.toggle(className);
+  }
+});
+
+
