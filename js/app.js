c299dcc80621c99e6fa12b6080669d4a1ee70a82
momenasuallam <59420500+momenasuallam@users.noreply.github.com>
2021-11-20 17:08:00 +0500

    All pages completed!

--- js/app.js
+++ js/app.js
@@ -5,12 +5,35 @@
 $(document).ready(function () {
   // Toggle menu on click
   $("#menu-toggler").click(function () {
-    toggleBodyClass("menu-active");
+    // toggleBodyClass("menu-active");
+    document.body.classList.toggle("menu-active");
+    document.body.classList.toggle("overflowhidden");
+    $("body").toggleClass("active");
+    $(".dflex1 img").toggleClass("active");
+    $(".col2dflex1").toggleClass("active");
   });
 
-  function toggleBodyClass(className) {
-    document.body.classList.toggle(className);
-  }
+  // function toggleBodyClass(className) {
+  //   document.body.classList.toggle(className);
+  // }
 });
 
+var shows = document.querySelectorAll(".show");
+var infos = document.querySelectorAll(".info");
+var accordions = document.querySelectorAll(".accordion");
 
+for (let i = 0; i < shows.length; i++) {
+  shows[i].onclick = function () {
+    for (let j = 0; j < shows.length; j++) {
+      if (shows[j] != this) {
+        infos[j].classList.remove("reveal");
+        shows[j].innerHTML = "+";
+        accordions[j].classList.remove("radius");
+      }
+    }
+    accordions[i].classList.toggle("radius");
+    infos[i].classList.toggle("reveal");
+    if (infos[i].className.includes("reveal")) shows[i].innerHTML = "-";
+    else shows[i].innerHTML = "+";
+  };
+}
